8b24f29c9efd6e1067416e09abc0194c21a7706a
Ubuntu <ubuntu@ip-172-31-10-11.ap-south-1.compute.internal>
2025-02-24 04:20:52 +0000

    frontend file App.js

---
+++ frontend/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} - {user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
